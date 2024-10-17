4da36432e0a897eaa03ec22fdc1d163193cd7dfa
íguswlsdl0121 <guswlsdl0121@gmail.com>
2024-10-17 12:00:22 +0900

    build: Spring  nitializer로 프로젝트 생성

---
+++ src/test/java/com/reboot_course/firstcome_system/FirstcomeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reboot_course.firstcome_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstcomeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
