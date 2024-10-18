23f98d5c618176624c61c4e67fc10ccba46c1094
íguswlsdl0121 <guswlsdl0121@gmail.com>
2024-10-18 13:50:57 +0900

    feat: ProductRepository에 커서기반 페이지네이션 쿼리 추가

--- src/main/java/com/reboot_course/firstcome_system/product/repository/ProductRepository.java
+++ src/main/java/com/reboot_course/firstcome_system/product/repository/ProductRepository.java
@@ -1,7 +1,22 @@
 package com.reboot_course.firstcome_system.product.repository;
 
+import com.reboot_course.firstcome_system.product.dto.response.ProductDTO;
 import com.reboot_course.firstcome_system.product.entity.Product;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("""
+            SELECT new com.reboot_course.firstcome_system.product.dto.response.ProductDTO(p.id, p.name, p.price)
+            FROM Product p
+            WHERE p.id < :cursor
+            ORDER BY p.id DESC
+            LIMIT :size
+            """)
+    List<ProductDTO> getProducts(@Param("size") Integer size, @Param("cursor") Integer cursor);
 }
